543e12816a700ed630191cb6d6840fd18f179324
Andrey.Sosnin <woodogul@yandex.ru>
2021-10-24 17:46:32 +0300

    refactoring: code style

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 export const getIndicatorName = ({ health }) => {
   if (health > 50) {
     return 'healthy';
-  } if (health > 15 && health <= 50) {
+  }
+  if (health > 15 && health <= 50) {
     return 'wounded';
   }
   return 'critical';
